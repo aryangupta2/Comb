ce7e0e5a041259b3fe2563cf78dd80ce103b3518
Michael <osujim2012@gmail.com>
2023-02-11 04:31:11 -0500

    Update scraper.py

--- backend/scraper.py
+++ backend/scraper.py
@@ -6,6 +6,7 @@ import os
 import time
 from decouple import config
 from selenium.webdriver.chrome.service import Service
+from fuzzywuzzy import fuzz
 
 class Scraper:
     def __init__(self):
@@ -18,5 +19,27 @@ class Scraper:
         self.browser = webdriver.Chrome(service=service, options=chrome_options)
 
     def scrape(self):
-        self.browser.get('https://google.com/')
-        print(self.browser.title)
+        browser = self.browser
+        product_name = 'Apple iPhone 13 128GB - Blue - Unlocked'
+        browser.get('https://www.amazon.com/s?k=' + product_name)
+        WebDriverWait(browser, 0.1)
+        
+        parent = browser.find_element(By.XPATH, "//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]")
+        elements = parent.find_elements(By.CLASS_NAME, "a-size-medium")
+        closest_element = elements[0]
+        highest_ratio = fuzz.ratio(product_name, closest_element.text)
+        for element in elements:
+            ratio = fuzz.ratio(product_name, element.text)
+            if ratio > highest_ratio:
+                closest_element = element
+                highest_ratio = ratio
+        print(closest_element.text)
+        closest_element.click()
+
+        WebDriverWait(browser, 0.1)
+        print(browser.title)
+        rating = browser.find_element(By.XPATH, "//*[@id=\"reviewsMedley\"]/div/div[1]/span[1]/span/div[2]/div/div[2]/div/span/span")
+        
+        WebDriverWait(browser, 0.1)
+        print(rating.text)
+
