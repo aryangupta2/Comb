aa18d47af6bf4a2f25f0528e2f8b1395f7f3be28
Michael <osujim2012@gmail.com>
2023-02-11 02:25:32 -0500

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from scraper import Scraper
 
 
 app = FastAPI()
+scraper = Scraper()
 
+class Review(BaseModel):
+    #logo: str
+    author: str
+    body: str
+
+class Item(BaseModel):
+    customer_avg: float
+    customer_reviews: List[Review] = []
+    #article_avg: float
 
 @app.get('/')
 def get():
+    scraper.scrape()
     return "Hello"
