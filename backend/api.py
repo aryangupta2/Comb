b83a238442b7112454cb8081df09d9a71c3fbfda
Michael <osujim2012@gmail.com>
2023-02-11 09:41:08 -0500

    Implement multithreading and look for top comments OR general comments for amazon

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-from scraper import Scraper, Review, SiteReview
+from scraper import Scraper, Review, SiteReview, scrape_amazon
+import threading
 
 
 app = FastAPI()
-scraper = Scraper()
+
+threadLocal = threading.local()
+
+def create_scraper():
+    scraper = getattr(threadLocal, 'scraper', None)
+    if scraper is None:
+        scraper = Scraper()
+        setattr(threadLocal, 'scraper', scraper)
+    return scraper
 
 class Item(BaseModel):
     customer_avg: float
     customer_reviews: List[Review] = []
     #article_avg: float
 
+class Product(BaseModel):
+    product_name: str
+
 @app.get('/')
-def get():
-    
-    return scraper.scrape_amazon('Apple iPhone 13 Pro 128GB - Blue - Unlocked')
+def get(product: Product):
+    scraper = create_scraper()
+    return scrape_amazon(scraper, product.product_name)
