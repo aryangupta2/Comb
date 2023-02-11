a5d1f1b3eb672fb02dd02b457465708a3cb17fb5
Michael <osujim2012@gmail.com>
2023-02-11 05:50:14 -0500

    Fetch the top positive review

--- backend/scraper.py
+++ backend/scraper.py
@@ -25,12 +25,32 @@ class Scraper:
         chrome_options.add_argument("--no-sandbox")
         self.browser = webdriver.Chrome(service=service, options=chrome_options)
 
+    def wait(self):
+        WebDriverWait(self.browser, 0.3)
+
+    def build_amazon_review(self, XPATH) -> Review:
+        review = self.browser.find_element(By.XPATH, XPATH)
+
+        rating_element = review.find_element(By.XPATH, ".//div[1]/div[4]/i/span")
+        rating_text = rating_element.get_attribute('textContent')
+        rating_float = float(rating_text[:3])
+
+        title_element = review.find_element(By.XPATH, ".//div[1]/div[4]/span[2]")
+        title_text = title_element.get_attribute('textContent')
+
+        read_more_element = review.find_element(By.XPATH, ".//div[2]/a")
+        review_hyperlink = read_more_element.get_attribute('href')
+        return Review(title=title_text, link=review_hyperlink, rating=rating_float)
+
     def scrape(self):
         browser = self.browser
+
+        # Search for the product
         product_name = 'Apple iPhone 13 128GB - Blue - Unlocked'
         browser.get('https://www.amazon.com/s?k=' + product_name)
-        WebDriverWait(browser, 0.1)
+        self.wait()
         
+        # Get all the products from the search and click on the one that matches the most with the product name
         parent = browser.find_element(By.XPATH, "//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]")
         elements = parent.find_elements(By.CLASS_NAME, "a-size-medium")
         closest_element = elements[0]
@@ -42,11 +62,25 @@ class Scraper:
                 highest_ratio = ratio
         print(closest_element.text)
         closest_element.click()
+        self.wait()
 
-        WebDriverWait(browser, 0.1)
+        # Find the rating for the product
         print(browser.title)
         rating = browser.find_element(By.XPATH, "//*[@id=\"reviewsMedley\"]/div/div[1]/span[1]/span/div[2]/div/div[2]/div/span/span")
-        
-        WebDriverWait(browser, 0.1)
-        print(rating.text)
+        self.wait()
+        rating_text = rating.text
+
+        # Go the the customer review page
+        see_all_reviews = browser.find_element(By.XPATH, "//*[@id=\"cr-pagination-footer-0\"]/a")
+        self.wait()
+        see_all_reviews.click()
+        self.wait()
+
+        # Select the top positive review
+        review: Review = self.build_amazon_review("/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[1]/div/div/div[1]/div[1]")
+        print(review)
+        # Select the top critical review
+        #critical_review = browser.find_element(By.XPATH,"//*[@id=\"viewpoint-RRMK4PYHPFNQF\"]/div[1]")
+
+
 
