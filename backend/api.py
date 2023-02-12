52fa65eaeb92354d8e9ac8915d5ba4130d3ddf08
Michael <osujim2012@gmail.com>
2023-02-12 07:29:22 -0500

    Add preloaded endpoint

--- backend/api.py
+++ backend/api.py
@@ -50,6 +50,67 @@ class Product(BaseModel):
 def get(product_name: str):
     return scrape_amazon(Scraper(), product_name)
 
+@app.get('/preloaded/')
+def get(product_name: str):
+    time.sleep(5)
+    return {
+    "stores": [
+        {
+            "site": "amazon",
+            "reviews": [
+                {
+                    "title": "Surprised me",
+                    "link": "https://www.amazon.com/gp/customer-reviews/R1NQPBC3OGEX0/ref=cm_cr_arp_d_rvw_ttl?ie=UTF8&ASIN=B09JFJ1Q5C",
+                    "rating": 5.0,
+                    "sentiment": "positive"
+                }
+            ],
+            "rating": 4.6
+        }
+    ],
+    "articles": [
+        {
+            "site": "rtings",
+            "link": "https://www.rtings.com/mouse/reviews/apple/magic-mouse-2",
+            "rating": 3.0
+        },
+        {
+            "site": "the-verge",
+            "link": "https://www.theverge.com/21524288/apple-iphone-12-pro-review",
+            "rating": 1.0
+        },
+        {
+            "site": "toms-guide",
+            "link": "https://www.tomsguide.com/reviews/iphone-14-pro",
+            "rating": 4.5
+        }
+    ],
+    "videos": [
+        {
+            "link": "https://www.youtube.com/watch?v=z_cazxB7We4",
+            "thumbnail_url": "https://i.ytimg.com/vi/z_cazxB7We4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAh2epNf4u8utJfo-uSIp6GMuf98w"
+        },
+        {
+            "link": "https://www.youtube.com/watch?v=K0L_9Ln331g",
+            "thumbnail_url": "https://i.ytimg.com/vi/K0L_9Ln331g/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA4fx7EBh3PgV9xek4rdTCr0LIB9Q"
+        },
+        {
+            "link": "https://www.youtube.com/watch?v=ybt03airuTk",
+            "thumbnail_url": "https://i.ytimg.com/vi/ybt03airuTk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBukg8LiYW3SnLbsH2nSwDvwhgE3w"
+        },
+        {
+            "link": "https://www.youtube.com/watch?v=nbX2_wL8Bb8",
+            "thumbnail_url": "https://i.ytimg.com/vi/nbX2_wL8Bb8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgknpWi_ZlW28EufmTv9H6UD7e3w"
+        },
+        {
+            "link": "https://www.youtube.com/watch?v=71m3SDGa220",
+            "thumbnail_url": "https://i.ytimg.com/vi/71m3SDGa220/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqQIgCjWZ9vtiiIF5UGfJYRVkuCA"
+        }
+    ],
+    "article_average": 2.8333333333333335,
+    "customer_average": 4.6
+}
+
 @app.get('/ratings/')
 async def get(product_name: str):
     with concurrent.futures.ThreadPoolExecutor() as executor:
