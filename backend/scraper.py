3b682b466c2f6f787332376e6f362744224faa1f
Michael <osujim2012@gmail.com>
2023-02-12 02:06:40 -0500

    Update scraper.py

--- backend/scraper.py
+++ backend/scraper.py
@@ -19,7 +19,7 @@ class Scraper:
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-        chrome_options.add_argument("start-maximized")
+        #chrome_options.add_argument("start-maximized")
 
         caps = DesiredCapabilities().CHROME
         caps["pageLoadStrategy"] = "none"
